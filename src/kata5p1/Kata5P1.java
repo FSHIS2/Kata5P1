ffbc183497010f9de6d25509ac0e4eb267cd6c65
Hodie <Hodie@LAPTOP-2K2BAPSH>
2018-11-28 17:36:17 +0100

    Kata5P1 versión 1

---
+++ src/kata5p1/Kata5P1.java
@@ -0,0 +1,9 @@
+package kata5p1;
+import java.sql.SQLException;
+public class Kata5P1 {
+
+    public static void main(String[] args) throws SQLException {
+        SelectApp app = new SelectApp();
+        app.selectAll();
+    }
+}
