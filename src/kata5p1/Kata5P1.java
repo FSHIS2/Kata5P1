e03599d3cd3a87dab6de8b35fd6af71ace7a0f99
FSHIS2 <>
2018-11-29 12:00:25 +0000

    Kata5P1 versión 2

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,9 +1,27 @@
 package kata5p1;
 import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 public class Kata5P1 {
 
     public static void main(String[] args) throws SQLException {
         SelectApp app = new SelectApp();
         app.selectAll();
+        createNewTable();
+    }
+    public static void createNewTable(){
+        String url = "jdbc:sqlite:email.db";
+        String sql = "CREATE TABLE IF NOT EXISTS dir_email(\n"
+                + "Id integer PRIMARY KEY AUTOINCREMENT,\n"
+                + "Mail text NOT NULL);";
+        try{
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+            System.out.println("Table created");
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
